a3b3d653821be754ca8e6bc91d01817128b14ebe
Ben Gamari <bgamari.foss@gmail.com>
2014-05-18 21:42:29 -0400

    Things work

--- Filter.hs
+++ Filter.hs
@@ -1,11 +1,11 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, TemplateHaskell, RankNTypes #-}
 import Prelude hiding (FilePath)
 
 import Control.Error
 import Control.Monad.Trans.Class
 import Control.Monad.State
 import Control.Monad.IO.Class
-import Data.Monoid ((<>))
+import Data.Monoid
 import Text.Pandoc
 import Text.Pandoc.Walk
 import Data.Default
@@ -13,10 +13,22 @@ import Control.Monad
 import qualified Data.Text as T
 import Filesystem.Path.CurrentOS
 import System.Process
+import Control.Lens
+
+import qualified Data.Map as M
+import qualified Data.Set as S
 
 import Inkscape
 
-main = runEitherT $ flip evalStateT 0 $ do
+data FilterState = FilterState { _figNum :: Int
+                               , _visLayers :: M.Map FilePath (S.Set LayerLabel)
+                               }
+makeLenses ''FilterState
+
+instance Default FilterState where
+    def = FilterState {_figNum = 0, _visLayers = mempty}
+
+main = runEitherT $ flip evalStateT def $ do
         liftIO getContents
     >>= return . readJSON def
     >>= walkM walkFilters
@@ -29,26 +41,38 @@ mapFileName f fpath = (directory fpath <> fname') `addExtensions` extensions fpa
   where fname' = fromText $ f $ either (error "invalid file name") id $ toText
                $ dropExtensions $ filename fpath
 
-walkFilters :: Inline -> StateT Int (EitherT String IO) Inline 
+visLayersFor :: FilePath -> Lens' FilterState (S.Set LayerLabel)
+visLayersFor fname = visLayers . at fname . non S.empty
+
+walkFilters :: Inline -> StateT FilterState (EitherT String IO) Inline 
 walkFilters (Image contents (fname,alt)) = do
-    let (contents', filters) = partitionEithers $ map findFilterDef contents
-    modify (+1)
+    (contents', filters) <- partitionEithers `fmap` mapM findFilterDef contents
+    figNum += 1
     case filters of
       [] -> return $ Image contents (fname, alt)
       _  -> do
-        n <- get
+        n <- use figNum
         let f = foldl (.) id filters
-        let fname' = decodeString fname
-            fnameNew = mapFileName (<>T.pack ("-"++show n)) fname'
+        let fnameNew = mapFileName (<>T.pack ("-"++show n)) fname'
         lift $ runFilter fname' fnameNew f
         return $ Image contents' (encodeString fnameNew, alt)
   where
-    findFilterDef :: Inline -> Either Inline SvgFilter
+    fname' = decodeString fname
+    findFilterDef :: Monad m => Inline -> StateT FilterState (EitherT String m) (Either Inline SvgFilter)
     findFilterDef (Code _ s) | "filter:":rest <- words s =
         case rest of
-          "only":layers -> Right $ showOnlyLayers (map T.pack layers)
-          x             -> error "unknown filter type"
-    findFilterDef x = Left x
+          "only":layers -> do let layers' = map T.pack layers
+                              visLayersFor fname' .= S.fromList layers'
+                              return $ Right $ showOnlyLayers layers'
+          "last":layers -> do
+            let layers' = map T.pack layers
+                adds = S.fromList $ mapMaybe ("+" `T.stripPrefix`) layers'
+                dels = S.fromList $ mapMaybe ("-" `T.stripPrefix`) layers'
+            visLayersFor fname' %= \xs->xs `S.union` adds `S.difference` dels
+            vis <- use $ visLayersFor fname'
+            return $ Right $ showOnlyLayers $ S.toList vis
+          x:_ -> lift $ left $ "unknown filter type: "<>x
+    findFilterDef x = return $ Left x
 walkFilters inline = return inline
 
 svgToPdf :: Inline -> EitherT String IO Inline 
